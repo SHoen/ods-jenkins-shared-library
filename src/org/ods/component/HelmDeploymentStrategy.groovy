0a4a4b16023f8b301fb96c1bb28be62e1aa9fdae
Steffen Höning <steffen.hoening@boehringer-ingelheim.com>
2024-03-13 15:03:28 +0000

    specify keyring file in addtion

--- src/org/ods/component/HelmDeploymentStrategy.groovy
+++ src/org/ods/component/HelmDeploymentStrategy.groovy
@@ -120,9 +120,7 @@ class HelmDeploymentStrategy extends AbstractDeploymentStrategy {
             """
             jenkins.maybeWithPrivateKeyCredentials(options.helmPrivateKeyCredentialsId) { String pkeyFile ->
                 if (pkeyFile) {
-                    steps.sh(script: "gpg --batch --import ${pkeyFile}", label: 'Import private key into keyring')
-                    steps.sh(script: "gpg --batch --keyring secring.gpg --export-secret-keys > ~/.gnupg/secring.gpg", label: 'Create secring file for old gpg version')
-
+                    steps.sh(script: "gpg --batch --secret-keyring secring.gpg --import ${pkeyFile}", label: 'Import private key into keyring')
                 }
 
                 // we add two things persistent - as these NEVER change (and are env independent)
