51b92ab2bab7ed4cf9c16ac93dc6fb0bcc906a3f
Steffen Höning <steffen.hoening@boehringer-ingelheim.com>
2024-03-13 15:31:47 +0000

    gpg import into secring.gpg

--- src/org/ods/component/HelmDeploymentStrategy.groovy
+++ src/org/ods/component/HelmDeploymentStrategy.groovy
@@ -122,9 +122,9 @@ class HelmDeploymentStrategy extends AbstractDeploymentStrategy {
                 if (pkeyFile) {
                     // steps.sh(script: "mkdir ~/.gnupg && echo ${pkeyFile} >> ~/.gnupg/secring.gpg", label: 'Import private key into keyring')
                     // steps.sh(script: "gpg --batch --allow-secret-key-import --import ${pkeyFile}", label: 'Import private key into keyring')
-                    steps.sh(script: "gpg --version", label: 'gpg version')
-                    steps.sh(script: "gpg-agent --version", label: 'gpg version')
-                    steps.sh(script: "sops --version", label: 'gpg version')
+                    steps.sh(script: "gpg --no-default-keyring --keyring secring.gpg --import  ${pkeyFile}", label: 'gpg version')
+                    // steps.sh(script: "gpg-agent --version", label: 'gpg version')
+                    // steps.sh(script: "sops --version", label: 'gpg version')
                 }
 
                 // we add two things persistent - as these NEVER change (and are env independent)
